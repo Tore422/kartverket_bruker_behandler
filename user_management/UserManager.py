05cdb05b2b08b39253ff4e8fd18c6102f46054a0
Tore Berven <tore.berven@gmail.com>
2023-05-03 00:40:23 +0200

    Made it possible to register new users
    * Implemented a function that registers a given user in the database.
      Currently only adds a premade test user, and does not
      accept the creation of new users through the CLI.
    
    * Refactored the code to increase readability,
      by splitting the main program code into functions.

--- user_management/UserManager.py
+++ user_management/UserManager.py
@@ -2,58 +2,52 @@ from User import User
 from UserInteraction import UserInteraction
 from DatabaseHandler import *
 
+def register_user_in_the_database(user):
+    names = ["name", "date_of_birth", "phone_number", "email_address"]
+    value_types = ["%s", "%s", "%s", "%s"]
+    values = [user.name, user.date_of_birth, user.phone_number, 
+            user.email_address]
+    try:
+        insert_into_table(TABLE_NAME, names, value_types, values)
+    except:
+        print("Something went wrong while trying to register "\
+              "the user in the database.")
+        raise
+    else:
+        print("Successfully registered the user in the database.")
 
+def perform_selected_operation(selected_operation):
+    if int(selected_operation) == 1:
+        for user in get_all_rows_from_database(TABLE_NAME):
+            print(user)
+    elif int(selected_operation) == 2:
+        register_user_in_the_database(user2)
+    elif int(selected_operation) == 3:
+        # Retrieve users from database matching search criteria
+        print("option 3")
+    else:
+        # Delete user from database matching criteria
+        print("option 4")
 
-    
-
-
-
-
-
-
+def start_program():
+    done = False
+    while not done:
+        print("")  # Separates the console output to make it easier to read
+        selected_operation = UserInteraction.ask_user_what_operation_to_perform()
+        if selected_operation == "q":
+            done = True
+        elif selected_operation.isdigit() and int(selected_operation) in\
+            range(1,5):
+            perform_selected_operation(selected_operation)
+        else:
+            print(("Did not recognize user input, please try again "
+                "or press q to quit."))
 
 
 user1 = User("John Smith", "01.01.1999", "12 34 56 78", "smith@mymail.com")
 user2 = User("Ola Nordmann", "22.06.2002", "15 34 56 79", "ola@mymail.com")
 
-users = []
-
-users.append(user1)
-users.append(user2)
-
-for user in users:
-    print(user)
-
-
 DATABASE_NAME = "mytestdatabase"
 TABLE_NAME = "users"
 
-
-done = False
-while not done:
-    selected_operation = UserInteraction.ask_user_what_operation_to_perform()
-    if selected_operation == "q":
-        done = True
-    elif selected_operation.isdigit() and int(selected_operation) in\
-        range(1,5):
-        print("was valid")
-        if int(selected_operation) == 1:
-            for user in get_all_rows_from_database(TABLE_NAME):
-                print(user)
-        elif int(selected_operation) == 2:
-            # Create a new user and register in database if possible
-            print("option 2")
-        elif int(selected_operation) == 3:
-            # Retrieve users from database matching search criteria
-            print("option 3")
-        else:
-            # Delete user from database matching criteria
-            print("option 4")
-    else:
-        print(("Did not recognize user input, please try again "
-            "or press q to quit.\n"))
-
-
-
-
-
+start_program()
